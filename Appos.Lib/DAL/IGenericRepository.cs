109c092e1dd61b4881c950e62c51b237349a6611
Bo Henriksen <stumpdenmark@hotmail.com>
2023-05-01 22:46:13 +0200

    Add GenericRepository

---
+++ Appos.Lib/DAL/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appos.Lib.DAL
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(int Id);
+        int Add(TEntity entity);
+        TEntity Update(TEntity entity);
+        TEntity Delete(TEntity entity);
+    }
+}
