6201c0f27a2617becd4169e461fbf014496b80ac
Bo Henriksen <stumpdenmark@hotmail.com>
2023-05-03 20:34:57 +0200

    Update CustomersController.cs
    Change return types to IResult

--- Appos.API/Controllers/CustomersController.cs
+++ Appos.API/Controllers/CustomersController.cs
@@ -19,21 +19,24 @@ namespace appos.Controllers
 
         // GET: api/<ValuesController>
         [HttpGet]
-        public IEnumerable<Customer> Get()
+        [ProducesResponseType(typeof(List<Customer>), StatusCodes.Status200OK)]
+        public IResult Get()
         {
-            return customerUnitOfWork.CustomerRepository.GetAll();
+            return Results.Ok(customerUnitOfWork.CustomerRepository.GetAll());
         }
 
         // GET api/<ValuesController>/5
         [HttpGet("{id}")]
-        public Customer Get(int id)
+        [ProducesResponseType(typeof(Customer), StatusCodes.Status200OK)]
+        public IResult Get(int id)
         {
-            return customerUnitOfWork.CustomerRepository.Get(id);
+            return Results.Ok(customerUnitOfWork.CustomerRepository.Get(id));
         }
 
         // POST api/<ValuesController>
         [HttpPost]
-        public StatusCodeResult Post([FromBody] Customer customer)
+        [ProducesResponseType(typeof(Customer), StatusCodes.Status201Created)]
+        public IResult Post([FromBody] Customer customer)
         {
             //TODO: Validate input
             try
@@ -41,45 +44,54 @@ namespace appos.Controllers
                 var newId = customerUnitOfWork.CustomerRepository.Add(customer);
                 customerUnitOfWork.CustomerLogRepository.Add(new CustomerLog() { Event = "Add", Details = customer.ToString(), Created = DateTime.Now, CustomerId = newId });
                 customerUnitOfWork.Commit();
+                return Results.Created<Customer>($"/customers/{newId}",customerUnitOfWork.CustomerRepository.Get(newId));
             }
             catch (Exception ex)
             {
                 customerUnitOfWork.Rollback();
-                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+                return Results.Problem("Could not add customer: " + ex.Message);
             }
-
-            return new StatusCodeResult(StatusCodes.Status200OK);
         }
 
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
-        public StatusCodeResult Put(int id, [FromBody] Customer customer)
+        [ProducesResponseType(typeof(Customer), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IResult Put(int id, [FromBody] Customer customer)
         {
             //TODO: Validate input
             //TODO: Validate/use id
             var customerToUpdate = customerUnitOfWork.CustomerRepository.Get(customer.Id);
             if (customerToUpdate == null)
             {
-                return new StatusCodeResult(StatusCodes.Status400BadRequest);
+                //TODO: 
+                return Results.Problem("problem");
+            }
+
+            if (customer.Equals(customerToUpdate))
+            {
+                return Results.Ok(customerToUpdate);
             }
 
             try
             {
-                customerUnitOfWork.CustomerRepository.Update(customer);
+                var updatedCustomer = customerUnitOfWork.CustomerRepository.Update(customer);
                 customerUnitOfWork.CustomerLogRepository.Add(new CustomerLog() { Event = "Update", Details = customer.ToString(), Created = DateTime.Now, CustomerId = id });
                 customerUnitOfWork.Commit();
+                return Results.Ok(updatedCustomer);
             }
             catch (Exception ex)
             {
                 customerUnitOfWork.Rollback();
+                return Results.Problem("Could not update customer: " + ex.Message);
             }
-
-            return new StatusCodeResult(StatusCodes.Status200OK);
         }
 
         // DELETE api/<ValuesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        [ProducesResponseType(typeof(Customer), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IResult Delete(int id)
         {
             var customer = customerUnitOfWork.CustomerRepository.Get(id);
 
@@ -90,10 +102,12 @@ namespace appos.Controllers
                 customerUnitOfWork.CustomerRepository.Delete(customer);
                 customerUnitOfWork.CustomerLogRepository.Add(new CustomerLog() { Event = "Delete", Details = customer.ToString(), Created = DateTime.Now, CustomerId = id });
                 customerUnitOfWork.Commit();
+                return Results.Ok();
             }
             catch (Exception ex)
             {
                 customerUnitOfWork.Rollback();
+                return Results.Problem("Could not delete customer: " + ex.Message);
             }
         }
     }
