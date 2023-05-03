a7a17924b1d1493443a4cb8480ecdbefad44d3cc
Bo Henriksen <stumpdenmark@hotmail.com>
2023-05-03 20:34:21 +0200

    Update Customer.cs
    Add equals

--- Appos.Lib/Models/Customer.cs
+++ Appos.Lib/Models/Customer.cs
@@ -6,5 +6,18 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj == null) return false;
+            if(obj.GetType() != typeof(Customer)) return false;
+
+            Customer customer = (Customer)obj;
+
+            return  this.Name.Equals(customer.Name) &&
+                    this.Email.Equals(customer) &&
+                    this.Phone.Equals(customer.Phone);
+                    
+        }
     }
 }
